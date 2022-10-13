37caea00c7fb3d95bdf5fc93fdb0a9065d929bd2
231globus <231globus@gmail.com>
2022-10-14 02:24:47 +0300

    feat: add two chek

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+
+	let transArr = []
+	for(let i=0;i<arr.length;i++) {
+		if(arr[i] === '--discard-next'){
+			i++;
+		}
+		else if(arr[i] === '--discard-prev') {
+			if(arr[i] == 0) i++
+		 	transArr.splice(i-1, 1)
+		}
+		else if(arr[i] === '--double-next') {
+			
+		}
+		else if(arr[i] === '--double-prev') {
+			
+		}
+		else {
+			transArr.push(arr[i])
+		}
+		
+	}
+	return transArr
 }
+
+console.log(transform([ '--discard-prev', 1, 2, 3,'--discard-prev', 4, 5,  6, 7, '--discard-prev']))
 
 module.exports = {
   transform
